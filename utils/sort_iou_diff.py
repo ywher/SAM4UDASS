ac8a8e68eb17b95585f3a41c339c6a11c5dbd9b3
ywher <958118196@qq.com>
2023-07-03 12:17:38 +0800

    update gta config

--- utils/sort_iou_diff.py
+++ utils/sort_iou_diff.py
@@ -37,8 +37,8 @@ def sort_csv_files(folder_path, column_name, index, save_path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='CSV文件排序工具')
     parser.add_argument('--folder_path', type=str, help='文件夹路径', \
-                        default='/media/ywh/1/yanweihao/projects/segmentation/segment-anything/outputs/cityscapes/train_fusion_syn_daformer_f4_new2/ious')
-    parser.add_argument('--column_name', type=str, help='需要统计的列名称', default='Differ_4_0')
+                        default='/media/ywh/1/yanweihao/projects/segmentation/segment-anything/outputs/cityscapes/debug_syn_tmp2/ious')
+    parser.add_argument('--column_name', type=str, help='需要统计的列名称', default='Differ_5_0')
     parser.add_argument('--index', type=int, help='需要统计的索引序号', default=0)
     parser.add_argument('--save_path', type=str, help='保存结果的路径,输入为文件名', default='result.csv')
     args = parser.parse_args()
