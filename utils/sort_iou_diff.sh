7299a59b55818d8595d82da2e1a2addd2a6d7274
ywher <958118196@qq.com>
2023-08-08 12:22:53 +0800

    update util

---
+++ utils/sort_iou_diff.sh
@@ -0,0 +1,6 @@
+python sort_iou_diff.py \
+--folder_path "/media/ywh/1/yanweihao/projects/segmentation/segment-anything/outputs/ACDC/tufl_acdc/ious" \
+--column_name "Differ_5_0" \
+--index 0 \
+--save_path "Differ_5_0.csv" \
+--order 0
